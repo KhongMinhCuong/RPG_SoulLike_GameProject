f49f760b75ac286d4f202199713790a4b3594284
hungvuvinh <vuvinhhungtq03@gmail.com>
2025-12-16 04:14:29 +0700

    Bổ sung cách tấn công

--- rl-game-project/enemy/scripts/states/enemy_attack.gd
+++ rl-game-project/enemy/scripts/states/enemy_attack.gd
@@ -1,6 +1,17 @@
 extends State
 class_name EnemyAttack
 
+enum AttackType {
+	MELEE,
+	RANGED
+}
+
+@export var attack_type: AttackType = AttackType.MELEE
+
+# chỉ dùng khi bắn vật thể
+@export var projectile_spawn: Node2D
+var projectile_scene = preload("res://enemy/scenes/arrow.tscn")
+
 @onready var animation_player: AnimationPlayer = $"../../AnimationPlayer"
 @onready var animated_sprite: AnimatedSprite2D = $"../../AnimatedSprite2D"
 @onready var hitbox: Hitbox = $"../../Hitbox"
@@ -8,17 +19,14 @@ class_name EnemyAttack
 var monster: CharacterBody2D
 var player: Node2D
 var attack_timer: float = 0.0
+var is_locked := false
 
-func enter() -> void:
+func enter():
 	print("ATTACK")
 	monster = owner if owner else get_parent().owner
-	if not monster:
-		push_error("EnemyAttack: Monster reference is null!")
-		return
 	
 	player = monster.player
 	attack_timer = 0.0
-	monster.is_attacking = false
 	animated_sprite.play("idle")
 
 func exit() -> void:
@@ -43,21 +51,56 @@ func update(delta: float) -> void:
 		return
 
 	if not monster.is_attacking:
-		_perform_attack()
+		match attack_type:
+			AttackType.MELEE:
+				_melee_attack()
+			AttackType.RANGED:
+				_ranged_attack()
 
-func _perform_attack() -> void:
+func _melee_attack() -> void:
 	if monster.is_attacking:
 		return
 	
 	monster.is_attacking = true
 	attack_timer = monster.attack_cooldown
 	
-	hitbox.enable()
 	animation_player.play("attack")
 	
 	animated_sprite.flip_h = player.global_position.x < monster.global_position.x
 
 	await animation_player.animation_finished
-	hitbox.disable()
 	monster.is_attacking = false
 	animated_sprite.play("idle")
+
+func _ranged_attack() -> void:
+	if not projectile_scene or not projectile_spawn:
+		push_error("EnemyAttack: Missing projectile setup!")
+		return
+	if monster.is_attacking:
+		return
+	
+	monster.is_attacking = true
+	attack_timer = monster.attack_cooldown
+
+	animated_sprite.flip_h = player.global_position.x < monster.global_position.x
+	animated_sprite.play("attack")
+
+	await animated_sprite.animation_finished
+
+	_spawn_projectile()
+
+	monster.is_attacking = false
+	animated_sprite.play("idle")
+
+func _spawn_projectile():
+	var projectile = projectile_scene.instantiate()
+	projectile.global_position = projectile_spawn.global_position
+	projectile.direction = monster.direction
+
+	# Optional interface
+	if projectile.has_method("set_direction"):
+		projectile.set_direction(
+			sign(player.global_position.x - monster.global_position.x)
+		)
+
+	get_tree().current_scene.add_child(projectile)
